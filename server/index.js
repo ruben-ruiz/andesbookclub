0d8a63c4dd65bf36bd9702018bf9d4cfa4f7fcf8
rriegel <ryan.j.riegel@gmail.com>
2021-05-20 20:09:09 -0400

    Started to build application skeleton. npm packages were installed

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const axios = require('axios');
+require('dotenv').config();
+
+const app = express();
+app.use(bodyparser.json());
+// app.use(express.static('client/dist'));
+
+
+app.get('/', function (req, res) {
+  res.status(200).send('Request Successful')
+})
+
+
+const PORT = 8080;
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
