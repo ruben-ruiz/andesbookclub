ec897fa3664bfd91f49546c562a7c7b5f755f59b
rriegel <ryan.j.riegel@gmail.com>
2021-05-21 19:59:39 -0400

    adjusted lint ignore and git ignore files moved React DOM rendering to a separate page from App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 // import axios from 'axios';
 
 // eslint-disable-next-line react/prefer-stateless-function
@@ -11,5 +10,4 @@ class App extends React.Component {
   }
 }
 
-// eslint-disable-next-line no-undef
-ReactDOM.render(<App />, document.getElementById('app'));
+export default App;
