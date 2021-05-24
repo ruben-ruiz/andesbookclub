e5025c9f76152fa0e4a16daf8d61cca7a0066350
Dalton Xue <dalousha@gmail.com>
2021-05-24 11:26:46 -0400

    Implemented googleLogout Component

---
+++ client/src/Logout.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { GoogleLogout } from 'react-google-login';
+
+const ClientId = 'AndesBookClub.apps.googleusercontent.com';
+
+function Logout() {
+  const onSuccess = () => {
+    console.log('Logout made successfully');
+    alert('Logout made successfully ✌');
+  };
+
+  return (
+    <div>
+      <GoogleLogout
+        clientId={clientId}
+        buttonText="Logout"
+        onLogoutSuccess={onSuccess}
+      ></GoogleLogout>
+    </div>
+  );
+}
+
+export default Logout;
