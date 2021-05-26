ac743594ab140cee8c55e46b0aaf731359363d38
Mark McLindon <clindo89@gmail.com>
2021-05-26 13:32:34 -0400

    replaced landing in app.jsx, removed bookInfo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 // import Login from './Login';
 // import Logout from './Logout';
-// import Landing from './components/Landing';
+import Landing from './components/Landing';
 
 // import axios from 'axios';
-import BookInfo from './components/bookInfo';
+// import BookInfo from './components/bookInfo';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends React.Component {
   render() {
     return (
       <div>
-        <BookInfo />
+        <Landing />
       </div>
     );
   }
