48dadf32f2bd57231814ead5fca1fa3e53ae52fd
Joelchrist Abreu <ajoelcrist@gmail.com>
2021-05-27 10:20:46 -0400

    Edited upvote route to just vote route

--- db/routes/questions/index.js
+++ db/routes/questions/index.js
@@ -4,11 +4,10 @@ const db = require('../../index');
 const questionsRouter = express.Router();
 const highestQuestionsRouter = require('./highest');
 const booksQuestionsRouter = require('./bookQuestions');
-const upvoteQuestionsRouter = require('./updateVotes');
+const voteQuestionsRouter = require('./updateVotes');
 
 const questionsTopUsersRouter = require('./topUsers');
 const questionsTopQuestionsRouter = require('./topQuestions');
-
 
 // root router for questions
 questionsRouter.get('/', (req, res) => {
@@ -18,7 +17,7 @@ questionsRouter.get('/', (req, res) => {
 
 questionsRouter.use('/highest', highestQuestionsRouter);
 questionsRouter.use('/book', booksQuestionsRouter);
-questionsRouter.use('/update', upvoteQuestionsRouter);
+questionsRouter.use('/update', voteQuestionsRouter);
 questionsRouter.use('/topusers', questionsTopUsersRouter);
 questionsRouter.use('/topquestions', questionsTopQuestionsRouter);
 
--- db/routes/questions/updateVotes.js
+++ db/routes/questions/updateVotes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const db = require('../../index');
-const upvoteQuestionsRouter = express.Router();
+const voteQuestionsRouter = express.Router();
 
 // root router for questions
-upvoteQuestionsRouter.put('/upvote/:question_id', (req, res) => {
+voteQuestionsRouter.put('/upvote/:question_id', (req, res) => {
   let question_id = req.params.question_id;
   let { upvote } = req.body;
   upvote = parseInt(upvote);
@@ -11,7 +11,7 @@ upvoteQuestionsRouter.put('/upvote/:question_id', (req, res) => {
     .then((data) => res.sendStatus(204));
 });
 
-upvoteQuestionsRouter.put('/downvote/:question_id', (req, res) => {
+voteQuestionsRouter.put('/downvote/:question_id', (req, res) => {
   let question_id = req.params.question_id;
   let { downvote } = req.body;
   downvote = Math.abs(parseInt(downvote));
@@ -19,4 +19,4 @@ upvoteQuestionsRouter.put('/downvote/:question_id', (req, res) => {
     .then((data) => res.sendStatus(204));
 });
 
-module.exports = upvoteQuestionsRouter;
+module.exports = voteQuestionsRouter;
