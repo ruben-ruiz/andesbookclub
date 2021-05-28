7739ec4a73ba873df66d23a039f8eef7fe40df5d
Ruben <ruben@rubens-MacBook-Pro.local>
2021-05-27 21:36:36 -0400

    reworked a few things on dashboard

--- db/routes/users/quizzes.js
+++ db/routes/users/quizzes.js
@@ -5,13 +5,10 @@ const userQuizzesRouter = express.Router();
 
 function getUserQuizzes(req) {
   return db.query(`
-    SELECT books.title, books.thumbnail FROM books
-    INNER JOIN (
-      SELECT * FROM questions
-      JOIN userBooks ON userBooks.bookId = questions.bookId
-      WHERE userBooks.userId = ${1}
-    )
-    ON books.id = userBooks.bookId
+    SELECT title, thumbnail
+    FROM books
+    JOIN userBooks ON userBooks.bookId = questions.bookId
+    WHERE userBooks.userId = ${1} AND books.id = userBooks.bookId
   `);
 }
 userQuizzesRouter.get('/', (req, res) => {
