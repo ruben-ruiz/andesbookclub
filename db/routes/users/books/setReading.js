a744647d05f368f7b2157fbbd1133cfe0fee6d29
Max Tickman <maxiant1994@gmail.com>
2021-05-28 09:24:32 -0500

    lots of fixes

--- db/routes/users/books/setReading.js
+++ db/routes/users/books/setReading.js
@@ -8,8 +8,8 @@ setReadingRouter.put('/:bookId', (req, res) => {
   let bookId = req.params.bookId;
   let userId = req.session.userId;
 
-  console.log('attempting to change to reading from completed');
-  console.log('userId, bookId ', userId, bookId);
+  // console.log('attempting to change to reading from completed');
+  // console.log('userId, bookId ', userId, bookId);
   db.query(`
     UPDATE userBooks
     SET isCompleted = false
