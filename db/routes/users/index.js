b9f75b6d0ec91dba901ba805dd40226b58330919
Dalton Xue <dalousha@gmail.com>
2021-05-28 09:21:05 -0400

    Resolved merge conflicts

--- db/routes/users/index.js
+++ db/routes/users/index.js
@@ -9,11 +9,8 @@ const db = require('../../index');
 const usersRouter = express.Router();
 const userLoginRouter = require('./login');
 const userLogoutRouter = require('./logout');
-<<<<<<< HEAD
 const userStatsRouter = require('./userStats')
-=======
 const userBooksRouter = require('./books');
->>>>>>> main
 
 // root router for users
 usersRouter.get('/', async (req, res) => {
@@ -21,10 +18,7 @@ usersRouter.get('/', async (req, res) => {
 });
 usersRouter.use('/login', userLoginRouter);
 usersRouter.use('/logout', userLogoutRouter);
-<<<<<<< HEAD
 usersRouter.use('/userStats', userStatsRouter);
-=======
 usersRouter.use('/books', userBooksRouter);
->>>>>>> main
 
 module.exports = usersRouter;
