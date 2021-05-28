55300ced70ef846d6a228ac4ffa0c62e25bc482f
Max Tickman <maxiant1994@gmail.com>
2021-05-28 10:06:01 -0500

    Fixed where clause in quizes

--- db/routes/users/quizzes.js
+++ db/routes/users/quizzes.js
@@ -6,21 +6,20 @@ const userQuizzesRouter = express.Router();
 function getUserQuizzes(req) {
   return db.query(`
 
-    SELECT books.title, books.bookId, books.thumbnail, MAX(bookQuestions.answeredAt)
-    FROM books
-    INNER JOIN(
-    SELECT answersPerBook.*
-    FROM userBooks
-    LEFT JOIN LATERAL (
-      SELECT questions.bookId, questions.questionId, MAX(userAnswers.answeredAt) as answeredAt
-      FROM userAnswers
-      JOIN questions ON questions.questionId = userAnswers.questionId
-    GROUP BY questions.questionId
-    ) as answersPerBook ON answersPerBook.bookId = userBooks.bookId
-      WHERE userBooks.isCompleted = true AND userBooks.userId = ${1}
-    ) as bookQuestions ON bookQuestions.bookId = books.bookId
-      WHERE DATE_PART('day', NOW() - bookQuestions.answeredAt) > 0
-    GROUP BY books.bookId
+  SELECT books.title, books.bookId, books.thumbnail, MAX(bookQuestions.answeredAt) as answeredAt
+  FROM books
+  INNER JOIN(
+  SELECT answersPerBook.*
+  FROM userBooks
+  LEFT JOIN LATERAL (
+    SELECT questions.bookId, questions.questionId, MAX(userAnswers.answeredAt) as answeredAt
+    FROM questions
+    LEFT JOIN userAnswers ON questions.questionId = userAnswers.questionId
+  GROUP BY questions.questionId
+  ) as answersPerBook ON answersPerBook.bookId = userBooks.bookId
+    WHERE userBooks.isCompleted = true AND userBooks.userId = ${req.session.userId}
+  ) as bookQuestions ON bookQuestions.bookId = books.bookId
+  GROUP BY books.bookId;
   `);
 }
 userQuizzesRouter.get('/', (req, res) => {
